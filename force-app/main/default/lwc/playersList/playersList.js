b58fa3e31e2fee17e74c713349979d5e12102a9a
Sergio Escribano Fernandez <sergio.escribano@nateevo.com>
2022-11-28 17:38:51 +0100

    ok

--- force-app/main/default/lwc/playersList/playersList.js
+++ force-app/main/default/lwc/playersList/playersList.js
@@ -1,28 +1,8 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire} from 'lwc';
+import getAllPlayer from '@salesforce/apex/PlayersServices.getAllPlayer'
 export default class PlayersList extends LightningElement {
 
-    players = [
-        {
-        Name: "Sergio",
-        Value__c: 7000000,
-        Image__c:"https://www.soyfutbol.com/__export/1630000963916/sites/debate/img/2021/08/26/mbappe_real_madrid_oficial_fichaje_psg_crop1630000597915.jpg_423682103.jpg",
-        Position__c: "Forward",
-        Team__c: "PSG"
-    }, 
-    {
-        Name: "pablo",
-        Value__c:5000000,
-        Image__c:"https://www.soyfutbol.com/__export/1630000963916/sites/debate/img/2021/08/26/mbappe_real_madrid_oficial_fichaje_psg_crop1630000597915.jpg_423682103.jpg",
-        Position__c: "defensa",
-        Team__c: "madrid"
-    }, 
-    {
-        Name: "Sergio",
-        Value__c: 7000000,
-        Image__c: "https://www.soyfutbol.com/__export/1630000963916/sites/debate/img/2021/08/26/mbappe_real_madrid_oficial_fichaje_psg_crop1630000597915.jpg_423682103.jpg",
-        Position__c: "portero",
-        Team__c: "barcelona"
-    }
-]
+  @wire(getAllPlayer) player;
+
+
 }
